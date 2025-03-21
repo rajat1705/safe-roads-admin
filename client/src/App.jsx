2a36e634e1209551c00eb9257d6d0df90e82c76d
37654 <rajat17rj@gmail.com>
2025-03-21 12:26:02 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import HomePage from './pages/HomePage/HomePage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/about" element={<div>Test</div>} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
 
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { IconReport, IconChartBar, IconAlertTriangle } from "lucide-react";
 
 export default function HomePage() {
   return (
     <>
       <Button
-        icon={<IconReport />}
         label="Reports"
         onClick={() => console.log("Reports button clicked")}
       />
