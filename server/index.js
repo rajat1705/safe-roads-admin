fe3bab872f4ece30bdce7fb214505b84c3f88c01
rajat1705 <rajat17rj@gmail.com>
2025-03-21 11:35:16 +0530

    init commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello from the backend!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
